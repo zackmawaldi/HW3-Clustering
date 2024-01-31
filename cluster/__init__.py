5fd95a5dfdf9ab185fef3004de4589bd4d2b88ea
zack <yazan.mawaldi@gmail.com>
2024-01-31 15:38:55 -0800

    apparently, I need to have a docstring on `__init__.py` to make it pip installable

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,7 @@
+"""
+A Python module for clustering data using k-means, and scoring them using Silhouette scoring.
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
